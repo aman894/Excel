52e268a4f68a9220647ef24237449c51fe7ecabc
aman894 <amandeep.malhotra.894@gmail.com>
2017-10-22 18:40:11 +0530

    init

---
+++ ExcelApplication/Module1.vb
@@ -0,0 +1,56 @@
+﻿Imports Excel = Microsoft.Office.Interop.Excel
+Module Module1
+    Public Class ExcelApplication
+        Private xlApp As Excel.Application
+        Private xlWorkbook As Excel.Workbook
+        Public Sub New()
+            xlApp = New Excel.Application
+            If xlApp Is Nothing Then
+                Console.WriteLine("Excel not installed!")
+            End If
+        End Sub
+
+        Public Function CreateWorkbook(workbookName As String) As Excel.Workbook
+            xlWorkbook = xlApp.Workbooks.Add()
+            xlWorkbook.SaveAs(workbookName)
+            'xlWorkbook.Close()
+            Return xlWorkbook
+        End Function
+
+        Public Function OpenWorkbook(workbookName As String) As Excel.Workbook
+            xlWorkbook = xlApp.Workbooks.Open(workbookName)
+            'xlWorkbook.Close()
+            Return xlWorkbook
+        End Function
+
+        Public Sub Insert(row As List(Of String))
+            Dim xlWorksheet As Excel.Worksheet = xlWorkbook.Worksheets(1)
+            Dim UsedRange = xlWorksheet.UsedRange
+            Dim RowRange = UsedRange.Rows
+            Dim targetRow As Int32 = RowRange.Count
+
+            If targetRow <> 1 Then
+                targetRow += 1
+            End If
+            xlWorksheet.Rows(1).Insert()
+            For i As Integer = 1 To row.Count
+                xlWorksheet.Cells(targetRow, i).value = row(i - 1)
+            Next
+            xlWorkbook.Save()
+        End Sub
+
+        Public Sub Close()
+            xlWorkbook.Close()
+        End Sub
+    End Class
+
+    Sub Main()
+        Dim ea As ExcelApplication = New ExcelApplication()
+        'Dim wb As Excel.Workbook = ea.CreateWorkbook("C:\Users\1305266\Desktop\workbook example\test.xlsx")
+        Dim wb As Excel.Workbook = ea.OpenWorkbook("C:\Users\1305266\Desktop\workbook example\test.xlsx")
+        Dim InputRow As New List(Of String) From {"4", "66", "3", "4"}
+        ea.Insert(InputRow)
+        ea.Close()
+    End Sub
+
+End Module
---
+++ ExcelApplication/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("ExcelApplication")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("ExcelApplication")>
+<Assembly: AssemblyCopyright("Copyright ©  2017")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("270c015d-673d-435a-8db2-de49a02df2b1")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
